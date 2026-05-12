214ba35b29f4c4969c3bfc0e24123e02313df437
franco20es-spec <francoroca@ejemplo.com>
2026-05-11 20:21:39 -0500

    mejoras en el estilo visual de alumno y login , ademas de creacion de modulos de profesor como prefil , notificaciones , horario, evalucaciones

--- sistema-educativo/src/app/core/config/menu-alumno.config.ts
+++ sistema-educativo/src/app/core/config/menu-alumno.config.ts
@@ -5,7 +5,6 @@ export const MENU_ALUMNO = [
   {
     section: 'Principal',
     items: [
-      { label: 'Dashboard', icon: 'bi bi-grid-1x2', route: 'alumno/dashboard' },
       { label: 'Mis Cursos', icon: 'bi bi-book', route: 'alumno/cursos' },
       { label: 'Mis Notas', icon: 'bi bi-file-earmark', route: 'alumno/notas' }
     ]
