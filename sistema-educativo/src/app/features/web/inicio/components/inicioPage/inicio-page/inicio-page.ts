3ab930dab43a14a129633dce184e61ac8ef7b9dd
DanielRM2 <danjod2004@gmail.com>
2026-05-31 10:51:48 -0500

    cambios del apartado inicio v2

--- sistema-educativo/src/app/features/web/inicio/components/inicioPage/inicio-page/inicio-page.ts
+++ sistema-educativo/src/app/features/web/inicio/components/inicioPage/inicio-page/inicio-page.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation, OnInit, Renderer2 } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-inicio-page',
   standalone: true, // Asumiendo Angular 17+
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './inicio-page.html',
   styleUrl: './inicio-page.css',
   encapsulation: ViewEncapsulation.None
